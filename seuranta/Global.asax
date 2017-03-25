36cef5c1cf55ac5acba63ab645855976ae004356
SpiritualEvolution <SpiritualEvolution@SPIRITUALEVOLUT>
2017-03-25 15:40:28 +0200

    Add project files.

---
+++ seuranta/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="seuranta.MvcApplication" Language="C#" %>
---
+++ seuranta/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(seuranta.Startup))]
+namespace seuranta
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
